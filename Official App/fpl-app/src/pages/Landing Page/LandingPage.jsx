cd848eeb158197778a54776030b8bdee7bc112a9
GuZuuu <morarated@gmail.com>
2025-05-05 17:40:15 +0300

    created a landing page/login page

--- Official App/fpl-app/src/pages/Landing Page/LandingPage.jsx
+++ Official App/fpl-app/src/pages/Landing Page/LandingPage.jsx
@@ -2,14 +2,14 @@ import { Link } from 'react-router-dom';
 
 import "../Landing Page/Landingpage.css";
 
-function LandingPage()  {
+const LandingPage = () => {
    return (
       <>
          <h1>WELCOME TO FPL</h1>
 
          <div>
             <div>Username: <input></input></div>
-            <div>Password: <input></input></div>
+            <div>Password: <input type='password'></input></div>
          </div>
 
          <Link to="/dashboard">
