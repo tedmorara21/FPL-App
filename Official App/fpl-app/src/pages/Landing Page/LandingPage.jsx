f2c362225fd59684d8925342a490850bf2da5d15
GuZuuu <morarated@gmail.com>
2025-05-12 22:12:36 +0300

    modified landing page with css and added alert logic

--- Official App/fpl-app/src/pages/Landing Page/LandingPage.jsx
+++ Official App/fpl-app/src/pages/Landing Page/LandingPage.jsx
@@ -1,26 +1,54 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import "../Landing Page/Landingpage.css";
 
 const LandingPage = () => {
-   return (
-      <>
-         <h1>WELCOME TO FPL</h1>
+   const [ username, setUsername ] = useState('');
+   const [ password, setPassword] = useState('');
 
-         <div>
-            <div>Username: <input></input></div>
-            <div>Password: <input type='password'></input></div>
+   const handleLogin = () => {
+      if ( username === '' || password === '' ) {
+         alert ("Enter both username and password");
+      }
+
+      // Continue with login logic here
+      console.log("Logging in with:", { username, password });
+   }
+
+   return (
+      <div className='landing-container'>
+         <h1>Welcome To FPL</h1>
+
+         <div className='input-group'>
+            <label htmlFor='username'>Username: </label>
+            <input 
+               type='text'
+               id='username'
+               name='username'
+               placeholder='Enter username as it is in FPL'
+            />
+         </div>
+
+         <div className='input-group'>
+            <label htmlFor='password'>Password: </label>
+            <input 
+               type='password'
+               id='password'
+               name='password'
+               placeholder='Enter your password'
+            />
          </div>
 
          <Link to="/dashboard">
-            <button>Login</button>
+            <button onClick={handleLogin}>Login</button>
          </Link>
 
          <Link to="/new-registration">
             <button>Register</button>
          </Link>
 
-      </>
+      </ div>
    )
 }
 
