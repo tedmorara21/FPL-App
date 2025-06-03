7e74c4f7e6a4ff76c5bbad79dee3ebacf787199b
GuZuuu <morarated@gmail.com>
2025-06-03 23:01:14 +0300

    added userContext to the profile header

--- Official App/fpl-app/src/components/ProfileHeader/ProfileHeader.jsx
+++ Official App/fpl-app/src/components/ProfileHeader/ProfileHeader.jsx
@@ -1,13 +1,22 @@
-import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+import { UserContext } from "../../assets/UserContext";
+
 import "./ProfileHeader.css";
 
-const ProfileHeader = ( { name, balance, points } ) => {
+const ProfileHeader = () => {
+   const { userData } = useContext(UserContext);
+
+   if (!userData) {
+      return null;
+   }
+
    return (
       <>
       <div className="profile-header" >
-         <span className="profile-item" >🏆 { name } </span> <br />
-         <span className="profile-item" >💰 { balance } shs</span> <br />
-         <span className="profile-item" >⭐ { points } points</span> <br />
+         <span className="profile-item" >🏆 { userData.team_name } </span> <br />
+         <span className="profile-item" >💰 { userData.balance } shs</span> <br />
+         <span className="profile-item" >⭐ { userData.total_points } points</span> <br />
       </div>
       </>
    )
