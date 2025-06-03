821532fcb98c07c3371d349e65b1ecb662d9a486
GuZuuu <morarated@gmail.com>
2025-06-03 22:20:21 +0300

    added an authenticateToken middleware to verify tokens

--- Official App/fpl-app/backend/server.js
+++ Official App/fpl-app/backend/server.js
@@ -191,7 +191,28 @@ server.post("/api/login", async ( req, res ) => {
 })
 
 
+// MIDDLEWARE TO VERIFY TOKENS
+function authenticateToken (req, res, next) {
+   const authHeader = req.headers['authorization']; 
 
+   if (!authHeader) {
+      return res.status(401).json( { message: "No token provided "} );
+   }
+
+   const token = authHeader.split(' ')[1];
+
+   if (!token) {
+      return res.status(401).json({ message: "Invalid token format" });
+   }
+
+   try {
+      const decodedToken = jwt.verify(token, secret_key);
+      req.user = decodedToken;
+      next();
+   } catch (err) {
+      return res.stautus(401).json({ message: "Invalid/expired token" })
+   }
+}
 
 
 //Start the server
