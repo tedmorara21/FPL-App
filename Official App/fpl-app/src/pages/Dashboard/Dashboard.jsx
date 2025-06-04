7b090af4a5ec97c0cd43026cb71bf180f73526ed
GuZuuu <morarated@gmail.com>
2025-06-04 22:44:58 +0300

    updated dashboard with userContext, as well as Profile header update to use real data

--- Official App/fpl-app/src/pages/Dashboard/Dashboard.jsx
+++ Official App/fpl-app/src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,8 @@
+import { useContext, useEffect, useState } from 'react'
 import { useNavigate } from "react-router-dom";
+
+import { UserContext } from '../../assets/UserContext';
+
 import ProfileHeader from "../../components/ProfileHeader/ProfileHeader";
 import HomeButton from "../../components/HomeButton/HomeButton";
 import "./Dashboard.css"
@@ -6,6 +10,15 @@ import "./Dashboard.css"
 const Dashboard = () => {
 
   const navigate = useNavigate();
+  const { userData } = useContext(UserContext);
+
+  if (!userData) {
+    return (
+      <p>
+        Loading...
+      </p>
+    )
+  }
 
    return (
      <>
@@ -15,27 +28,28 @@ const Dashboard = () => {
         </div>
 
        <h1>DASHBOARD</h1>
-       <div className="dashboard-welcome">Welcome to your dashboard <span className="highlight"> TEAMNAME </span>!</div>
-       <>
-        <ProfileHeader name={"Ted"} balance={1000} points={1783}/>
-       </>
+
+       <div className="dashboard-welcome">Welcome to your dashboard <span className="highlight"> {userData.team_name} </span>!</div>
+
+        <ProfileHeader />
 
         <div className="dashboard-section">
-          <p><strong>Profile Name : </strong> "PROFILENAME"</p> 
-          <p><strong>Team Name : </strong> "TEAMNAME" </p>
+          <p><strong>Profile Name : </strong> {userData.player_name} </p> 
+          <p><strong>Team Name : </strong> {userData.team_name} </p>
         </div>
 
         <div className="dashboard-section">
-          <p><strong>Balance = </strong>"0.00"</p>
+          <p><strong>Balance = </strong>{userData.balance}</p>
           <button 
               className="dashboard-button" 
               onClick={ () => navigate("/balance")}
-          >Withdraw</button> 
+          >Withdraw</button>
+          <p><strong>Total Points : </strong> {userData.money_earned} </p> 
         </div>
 
         <div className="dashboard-section">
-          <p><strong>Points this GW : </strong>"0"</p>
-          <p><strong>Total Points : </strong>"0"</p>
+          <p><strong>Points this GW : </strong> {userData.gw_points} </p>
+          <p><strong>Total Points : </strong> {userData.total_points} </p>
           <button 
               className="dashboard-button" 
               onClick={ () => navigate("/league")}
