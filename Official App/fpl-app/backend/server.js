3adf339d4dcfb7b1f06a45c73f5aa31ad8e85752
GuZuuu <morarated@gmail.com>
2025-06-09 00:05:12 +0300

    cleaned server and league-api

--- Official App/fpl-app/backend/server.js
+++ Official App/fpl-app/backend/server.js
@@ -20,30 +20,31 @@ const server = express();
 server.use(cors()); // ENABLE CORS
 server.use(express.json());  //TO PARSE JSON REQUEST BODY   
 
-//PROXY ENDPOINT
+//PROXY ENDPOINTS
+
+/*
+---.get("/api/league")
+---mongoose.connect = schemas = classes
+---.get("api/users")
+---.post("/api/login")
+---authenticateToken()
+---.get("/api/get-user-details")
+*/
 
 const API_URL = "https://fantasy.premierleague.com/api/leagues-classic/697909/standings/"; // 697909 for the specific league
 
 //GET USERS OF THE LEAGUE AND THEIR DATA
 server.get("/api/league", async ( req, res) => {
    try {
-      const response = await axios.get(API_URL, {
-         headers: { "User-Agent": "Mozilla/5.0" }, // Some APIs require a User-Agent
-       });
-
-      // console.log("Backend API Response:", response.data); // Debugging
-
-      res.json(response.data); //Forward the response
-   } catch (error) {
-      console.error("Error fetching data: by Guzuuu", error.message);
-      res.status(500).json({ error: "Error fetching data from the API" });
+      const response = await axios.get(API_URL);
+      res.json(response.data); 
+   } catch (err) {
+      res.status(500).json({ error: `Error fetching data from the API ${err}`});
    }
 });
 
-mongoose.connect('mongodb+srv://tedmorara21:tedmorara21@cluster0.jkn4gii.mongodb.net//',{
-   useNewUrlParser: true,
-   useUnifiedTopology: true,
-}).then( ()=> console.log('MongoDB connected') )
+mongoose.connect('mongodb+srv://tedmorara21:Whooshwhoosh.99@cluster0.jkn4gii.mongodb.net/database1')
+   .then( ()=> console.log('MongoDB connected') )
    .catch( err => console.error("MongoDB connection error", err) );
 
 
@@ -66,8 +67,8 @@ const duplicateUserSchema = new mongoose.Schema( {
    moneyEarned: Number
 } )
 
-const User = mongoose.model('Login Details', UserSchema);
-const DuplicateUser = mongoose.model("User Details", duplicateUserSchema, "UserDetails");
+const User = mongoose.model('Login Details', UserSchema, 'login details');
+const DuplicateUser = mongoose.model("User Details", duplicateUserSchema, "User Details");
 
 // GET USERS
 server.get("/api/users", async ( req, res ) => {
@@ -75,21 +76,26 @@ server.get("/api/users", async ( req, res ) => {
       const users = await User.find();
       res.status(200).json(users);
    } catch (error) {
-      console.error("Error fetching users by Guzuuu", error);
-      res.status(500).json( {
-         error: "Failed to fetch users"
-      } );
+      res.status(500).json({ error: "Failed to fetch users" });
    }
 })
 
 //ADD NEW USERS
 server.post("/api/users", async (req, res) => {
-   const { playerName, email, phoneNumber, password } = req.body;
+   const playerName = req.body.playerName;
+   const email = req.body.email;
+   const phoneNumber = req.body.phoneNumber;
+   const password = req.body.password;
+   // const { playerName, email, phoneNumber, password } = req.body;
 
    try {
       const existingUser = await User.findOne({
-          $or: [ { email }, { playerName } ]
+          $or: 
+          [ 
+            { email }, { playerName } 
+         ]
       });
+      // const existingUser = wait User.findOne({$or: [ {email} {playerName} ]});
       
       if (existingUser) {
          return res.status(400).json( { error: "Team name or email already registered" } )
@@ -106,22 +112,20 @@ server.post("/api/users", async (req, res) => {
 
       await newUser.save();
 
-      const fplResponse = await axios.get(API_URL, {
-         headers: { "User-Agent": "Mozilla/5.0" }
-      });
+      const fplResponse = await axios.get(API_URL);
 
-      let standings;
+      let fplStandings;
 
       if (!fplResponse.data.standings) {
-         standings = [];
+         fplStandings = [];
       } else {
-         standings = fplResponse.data.standings.results;
+         fplStandings = fplResponse.data.standings.results;
       }
 
-      const matchingPlayer = standings.find( player => player.player_name.toLowerCase() === playerName.toLowerCase() );
-      
+      // CHECK FOR A MATCHING PLAYER AND RETURN THE MATCHING PLAYER DETAILS
+      const matchingPlayer = fplStandings.find( player => player.player_name.toLowerCase() === playerName.toLowerCase() );
       if (!matchingPlayer) {
-         return res.json(404).json( { error: "Player name not found in league!" } );
+         return res.json(404).json({ error: "Player name not found in league!" });
       };
 
       const duplicateUser = new DuplicateUser ( {
@@ -138,18 +142,20 @@ server.post("/api/users", async (req, res) => {
       // console.log ("Duplicted user data: ", duplicateUser);
 
       res.status(201).json({ message: "User registered successfully"});
-   } catch ( error ){
-      console.error("Error saving user:", error)
-      res.status(500).json({ error: "Internal server error" });
+   } catch (err){
+      res.status(500).json({ error: `Internal server error, ${err}` });
    }
 })
 
 //LOGIN ENDPOINT
 server.post("/api/login", async ( req, res ) => {
-   const { playerName, password } = req.body;
+   const playerName = req.body.playerName;
+   const password = req.body.password;
+
+   // const { playerName, password } = req.body;
 
    try {
-      const player = await User.findOne( { playerName });
+      const player = await User.findOne( { playerName: playerName });
 
       if (!player) {
          return res.status(401).json( { message: "Team name not found by Guzuuu" } );
@@ -161,13 +167,13 @@ server.post("/api/login", async ( req, res ) => {
          return res.status(401).json( {message: "Incorrect username/password"} );
       };
 
-      //optional: return a token or user data
+      // RETURN  TOKEN OR USER DATA
       const token = jwt.sign(
          { id: player.playerName }, //PAYLOAD
-         secret_key,
+         secret_key, // SIGNATURE
          { expiresIn: "1h" }
       )
-      //console.loG(token);
+      //console.log(token);
 
       const decodedToken = jwt.verify(token, secret_key);
       //console.log("Decoded Token: ", decodedToken);
@@ -184,9 +190,8 @@ server.post("/api/login", async ( req, res ) => {
          decodedToken, decodedToken
       } );
 
-   } catch ( error ) {
-      console.error("Login error: ", error)
-      res.status(500).json( { message: "Internal Server error!" } );
+   } catch (err) {
+      res.status(500).json( { message: `Internal Server error! ${err}` } );
    }
 })
 
@@ -196,7 +201,7 @@ function authenticateToken (req, res, next) {
    const authHeader = req.headers['authorization']; 
 
    if (!authHeader) {
-      return res.status(401).json( { message: "No token provided "} );
+      return res.status(401).json({ message: "No token provided "});
    }
 
    const token = authHeader.split(' ')[1];
@@ -210,7 +215,7 @@ function authenticateToken (req, res, next) {
       req.user = decodedToken;
       next();
    } catch (err) {
-      return res.stautus(401).json({ message: "Invalid/expired token" })
+      return res.stautus(401).json({ message: `Invalid/expired token ${err}` })
    }
 }
 
@@ -224,10 +229,12 @@ server.get("/api/get-user-details", authenticateToken, async (req, res) => {
          return res.status(404).json( {message: "User not found"} );
       }
 
-      res.status(200).json( { message: "User not found" });
+      res.status(200).json( { 
+         message: "User not found",
+         user // returns user object
+       });
    } catch (err) {
-      console.error("Error fetching users: ", err);
-      res.status(500).json( { message: "Internal server error" } );
+      res.status(500).json( { message: `Internal server error ${err}` } );
    }
 } )
 
--- Official App/fpl-app/src/api/league-api.js
+++ Official App/fpl-app/src/api/league-api.js
@@ -1,24 +1,22 @@
 import axios from "axios";
 
-const API_URL = "https://fpl-proxy-server.onrender.com/api/league"; //local proxy
+const API_URL = "https://fpl-proxy-server.onrender.com/api/league"; 
 
 
 export const getLeagueStandings = async () => {
    try {
       const response = await axios.get(API_URL);
 
-      // console.log("Full API Response: ", response);
       // console.log("API Data: ", response.data);
 
       // Ensure API response has data before accessing
       if (response.data && response.data.standings && response.data.standings.results) {
          return response.data.standings.results; // Return users array
       } else {
-         console.error("Unexpected API response structure:", response.data);
          return []; // Return empty array if data is missing
       }
-   } catch (error) {
-      console.error("Error fetching league standings by Guzuuu" , error);
+   } catch (err) {
+      console.error("Error fetching league standings by Guzuuu" , err);
       return []; // Return empty array on error
    }
 }
