a7d491101f718e33d469fecc524852363f27ca36
tedmorara21 <tedmorara21@gmail.com>
2025-06-12 14:10:30 +0300

    Update Sidebar.jsx
    - added a toggleSidebar functionality that collapses the sidebar

--- Official App/fpl-app/src/components/Sidebar/Sidebar.jsx
+++ Official App/fpl-app/src/components/Sidebar/Sidebar.jsx
@@ -1,34 +1,52 @@
+import React, { useState } from "react"; 
 import { Link } from "react-router-dom";
 import { FiHome, FiUser, FiLogOut } from "react-icons/fi";
 import { FaTrophy } from "react-icons/fa";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  const [ collapsed, setCollapsed ] = useState(false);
+
+  function toggleSidebar () {
+    setCollapsed( prev => !prev );
+  }
+
   return (
-    <div className="sidebar">
-      <h2 className="sidebar-title">DASHBOARD</h2>
+    <div className={`sidebar ${collapsed ? "collapsed" : "" }`}>
+      <div className="toggle-btn onClick={toggleSidebar}">
+        ☰ Dashboard
+      </div>
       
       <ul>
+        
         <li>
           <Link to="dashboard/" className="sidebar-link">
-            <FiHome className="icon" /> Dashboard
+            <FiHome className="icon" /> 
+            <span className="link-text">Dashboard</span>
           </Link>
         </li>
+        
         <li>
           <Link to="/league" className="sidebar-link">
-            <FaTrophy className="icon" /> League
+            <FaTrophy className="icon" /> 
+            <span className="link-text">League</span>
           </Link>
         </li>
+        
         <li>
           <Link to="/balance" className="sidebar-link">
-            <FiUser className="icon" /> Balance
+            <FiUser className="icon" /> 
+            <span className="link-text">Balance</span>
           </Link>
         </li>
+        
         <li>
           <Link to="/login" className="sidebar-link">
-            <FiLogOut className="icon" /> Logout
+            <FiLogOut className="icon" /> 
+            <span className="link-text">Logout</span>
           </Link>
         </li>
+        
       </ul>
     </div>
   );
