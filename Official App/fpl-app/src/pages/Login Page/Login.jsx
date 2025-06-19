e54aa45a19a9f355a239c41c9c45c6b120518501
tedmorara21 <tedmorara21@gmail.com>
2025-06-19 11:07:02 +0300

    Update Login.jsx
    - changed authTOken to authToken

--- Official App/fpl-app/src/pages/Login Page/Login.jsx
+++ Official App/fpl-app/src/pages/Login Page/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
    const navigate = useNavigate();
 
    const handleLogout = () => {
-      localStorage.removeItem("authTOken");
+      localStorage.removeItem("authToken");
       navigate("/");
    }
 
