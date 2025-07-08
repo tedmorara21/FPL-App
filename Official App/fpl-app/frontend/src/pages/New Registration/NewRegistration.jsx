73cb61f4a1050d770aa7e6b95a76edbee2da21a2
tedmorara21 <tedmorara21@gmail.com>
2025-07-08 14:45:30 +0300

    Update NewRegistration.jsx

--- Official App/fpl-app/frontend/src/pages/New Registration/NewRegistration.jsx
+++ Official App/fpl-app/frontend/src/pages/New Registration/NewRegistration.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
  
 import "../New Registration/NewRegistration.css";
 
-import { checkUserExists } from '../../functions/check-user-exists';
+import { checkUserExists } from '../../utils/check-user-exists';
 
 const NewRegistration = () => {
    const navigate = useNavigate();
@@ -15,48 +15,58 @@ const NewRegistration = () => {
    const [ password, setPassword ] = useState('');
    const [ confirmPassword, setConfirmPassword ] = useState(''); 
 
-   const confirmPasswordFunction = async () => {
+   const [ successMessage, setSuccessMessage ] = useState("");
+   const [ errorMessage, setErrorMessage ] = useState("");
+   const [ phoneError, setPhoneError ] = useState("");
+   const [ passwordError, setPasswordError ] = useState("");
+   const [ usernameError, setUsernameError ] = useState("");
+
+   const registerUserFunction = async () => {
       // CHECK PHONE NUMBER LENGTH == 10 DIGITS
       if (phoneNumber.length !== 10) {
-         alert("Check phone number!");
+         setPhoneError("Check phone number");
+         setTimeout( () => setPhoneError(""), 2000 );
          return;
       }
 
       // CHECK IF PASSWORDS MATCH
       if ( password !== confirmPassword) {
-         alert("Passwords do not match!")
+         setPasswordError("Passwords do not match");
+         setTimeout( () => setPasswordError(""), 2000 );
          return;
       } 
 
       // CHECK IF PLAYERNAME EXISTS IN DATABASE
-
       try {
-         const response = await fetch("https://fpl-proxy-server.onrender.com/api/league");
+         const response = await fetch("http://localhost:5001/api/league"); // CHANGE LATER!!!!!!!!!!!!!!!!!!
          const data = await response.json();
 
          const leagueTeams = data.standings.results;
          const teamExists = leagueTeams.some( team => team.player_name === playerName );
 
          if (!teamExists) {
-            alert("No such team name found in the FPL league!");
+            setUsernameError("No such team name found in the FPL league!");
+            setTimeout( () => setUsernameError(""), 2000 );
+            return;
          } else {
             const userExists = await checkUserExists(playerName);
             
             if (userExists === true) {
-               alert("Player already registered!");
+               setUsernameError("Player already registered!");
+               setTimeout( () => setUsernameError(""), 2000 );
                return;
             } else if ( userExists === undefined ) {
-               alert("Server error.Try again later!");
+               setUsernameError("Server error. Try again later!");
+               setTimeout( () => setUsernameError(""), 2000 );
                return;
             }
 
             //IF MATCH IS FOUND, PROCEED
             const newRegistrationData = { playerName, email, phoneNumber, password };
-            // console.log("Registration Data: ", newRegistrationData);
 
             //SEND DATA TO MONGODB HERE
             try {
-               const res = await fetch("https://fpl-proxy-server.onrender.com/api/users", {
+               const res = await fetch("http://localhost:5001/api/users", { // CHANGE LATER!!!!!!!!!!!!!!!!!!
                   method: "POST",
                   headers: { "Content-Type": "application/json" },
                   body: JSON.stringify( { playerName, email, phoneNumber, password} ) 
@@ -64,30 +74,36 @@ const NewRegistration = () => {
 
                if (!res.ok) {
                   const errorData = await res.json();
-                  alert(`Registration Failed: ${errorData.error} || "Uknown error`);
+                  setErrorMessage(`Registration failed: ${errorData.error}`)
+                  setTimeout( () => setErrorMessage(""), 2000 );
                   return;
                }
 
                const data = await res.json();
 
                // RESET FORM
-               alert ("Account Created!");
+               setSuccessMessage("Account Created. Wait for redirect....");
+               setTimeout( () => {
+                  setSuccessMessage("");
+                  navigate("/");
+               }, 4000 );
 
                setPlayerName('');
                setEmail('');
                setPhoneNumber('');
                setPassword('');
                setConfirmPassword('');
-
-               //REDIRECT
-               navigate('/');
             } catch (err) {
-               console.error("Error saving user to Mongodb by Guzuuu ", err);
+               setErrorMessage("Error saving users to Mongodb by Guzuuu ", err); 
+               setTimeout( () => setErrorMessage(""), 2000 );
+               return;
             }
          }
       } catch (err) {
-         console.error("Error checking tem name ny Guzuuu:", err);
-         alert("Something went wrong. Please try again");
+         console.error("Error checking team name by Guzuuu:", err);
+         setErrorMessage("Something went wrong. Check console.");
+         setTimeout( () => setErrorMessage(""), 2000 );
+         return;
       }
    }
 
@@ -97,11 +113,11 @@ const NewRegistration = () => {
             <h1>New Registration</h1>
             <form className="new-registration-form" onSubmit={ (e) => {
                e.preventDefault();
-               confirmPasswordFunction();
+               registerUserFunction();
             } } >
 
                <div className='input-group'>
-               <label> Player Name </label>
+                  <label> Player Name </label>
                   <input 
                      type="text" 
                      value={playerName} 
@@ -109,10 +125,13 @@ const NewRegistration = () => {
                      placeholder="Name As It Is In FPL League" 
                      required
                   />
+                  { usernameError && (
+                     <span className='error-message'>{usernameError}</span>
+                  ) }
                </div>
 
                <div className='input-group'>
-               <label> Email Address </label>
+                  <label> Email Address </label>
                   <input 
                      type="email" 
                      value={email}
@@ -122,14 +141,19 @@ const NewRegistration = () => {
                </div>
 
                <div className='input-group'>
-               <label> Phone  Number (07) </label>
+                  <label> Phone  Number (07) </label>
                   <input 
                      type="text" 
                      value={phoneNumber} 
                      onChange={(e) => setPhoneNumber(e.target.value)}
                      required
                   />
+                  { phoneError && (
+                     <span className='error-message'>{phoneError}</span>
+                  ) }
                </div>
+
+               
 
                <div className='input-group'>
                <label> Password </label>
@@ -142,14 +166,25 @@ const NewRegistration = () => {
                </div>
 
                <div className='input-group'>
-               <label> Confirm Password </label>
+                  <label> Confirm Password </label>
                   <input 
                      type="password" 
                      value={confirmPassword}
                      onChange={(e) => setConfirmPassword(e.target.value)}
                      required
                   />
+                  { passwordError && (
+                     <span className='error-message'>{passwordError}</span>
+                  ) }
                </div>
+
+               { errorMessage && (
+                  <span className='error-message'>{errorMessage}</span>
+               ) }
+
+               { successMessage && (
+                  <span className='success-message'>{successMessage}</span>
+               ) }
 
                <>
                <div className='button-group'>
