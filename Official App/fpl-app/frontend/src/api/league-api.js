f774268616945cc67305949c33015e6e5be5ac0a
tedmorara21 <tedmorara21@gmail.com>
2025-07-08 14:29:37 +0300

    Update league-api.js

--- Official App/fpl-app/frontend/src/api/league-api.js
+++ Official App/fpl-app/frontend/src/api/league-api.js
@@ -1,22 +1,20 @@
 import axios from "axios";
 
-const API_URL = "https://fpl-proxy-server.onrender.com/api/league"; 
+const API_URL = "http://localhost:5001/api/league"; //  CHANGE LATER!!!!!!!!!!!!!!!!!!!!!
 
 
 export const getLeagueStandings = async () => {
    try {
       const response = await axios.get(API_URL);
-
-      // console.log("API Data: ", response.data);
-
-      // Ensure API response has data before accessing
+      
+      /* ENSURE API RESPONSE BEFORE ACCESSING */
       if (response.data && response.data.standings && response.data.standings.results) {
-         return response.data.standings.results; // Return users array
+         return response.data.standings.results; /* RETURN USERS ARRAY */
       } else {
-         return []; // Return empty array if data is missing
+         return [];
       }
    } catch (err) {
-      console.error("Error fetching league standings by Guzuuu" , err);
-      return []; // Return empty array on error
+      console.error("Error fetching league standings by Guzuuu:" , err);
+      return []; 
    }
 }
