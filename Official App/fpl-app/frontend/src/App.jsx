c504a11c1c33e6985a093b709727ae1e3e8c0895
tedmorara21 <tedmorara21@gmail.com>
2025-07-08 14:49:10 +0300

    Update App.jsx

--- Official App/fpl-app/frontend/src/App.jsx
+++ Official App/fpl-app/frontend/src/App.jsx
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
-
 import "normalize.css" // normalize styles for multiple browsers
 import './App.css'
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 
 import ProtectedRoute from "./components/ProtectedRoutes.jsx";
+import AdminRoute from './components/AdminRoute.jsx';
+
 import { UserProvider } from "./assets/UserContext.jsx";
 
 import Sidebar from "./components/Sidebar/Sidebar";
+
 import Dashboard from "./pages/Dashboard/Dashboard";
 import League from './pages/League/League.jsx';
 import Balance from "./pages/Balance/Balance.jsx";
 import Login from "./pages/Login Page/Login.jsx";
 import LandingPage from './pages/Landing Page/LandingPage.jsx';
 import NewRegistration from './pages/New Registration/NewRegistration.jsx';
+import Settings from './pages/Settings/Settings.jsx';
+
+import AdminDashboard from './admin/adminDashboard.jsx';
+import ManageUsers from './admin/manageUsers.jsx';
 
 function App() {
   const location = useLocation();
@@ -34,12 +40,25 @@ function App() {
       { showSidebar ? <Sidebar /> : null }
 
       {/* Main Content */}
-      <div className={`main-content ${ showSidebar ? "with-sidebar" : "full-width" }`}>
+      <div className={`main-content ${showSidebar ? "with-sidebar" : "full-width"} `}>
 
         <Routes>
           {/* Public Routes */}
           <Route path="/" element={< LandingPage />} />
           <Route path="/new-registration" element={< NewRegistration />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin/dashboard" element={
+            <AdminRoute>
+              <AdminDashboard />
+            </AdminRoute>
+          } />
+
+          <Route path="/admin/manage-users" element={
+            <AdminRoute>
+              <ManageUsers />
+            </AdminRoute>
+          } />
 
           {/* Protected Routes */}
           <Route path="/dashboard" element={
@@ -65,6 +84,12 @@ function App() {
               <ProtectedRoute> < Login /> </ProtectedRoute>
             </UserProvider>
           } />
+
+          <Route path='/settings' element={
+            <UserProvider>
+              <ProtectedRoute> <Settings /> </ProtectedRoute>
+            </UserProvider>
+          }/>
           
         </Routes>
       </div>
